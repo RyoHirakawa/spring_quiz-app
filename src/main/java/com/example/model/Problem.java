adb055d8344dc06f4d786a413ed955e6f390ee91
RyoHirakawa <ryo.hirakawa@g.dreamcareer.co.jp>
2024-05-22 14:37:44 +0900

    Problemクラスの選択肢をList<String>から4つのStringを持つ形に変更

--- src/main/java/com/example/model/Problem.java
+++ src/main/java/com/example/model/Problem.java
@@ -1,16 +1,12 @@
 package com.example.model;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 
@@ -24,10 +20,18 @@ public class Problem {
 	@NotBlank(message = "問題文を入力してください")
 	private String question;
 	
-	@NotEmpty
-	private java.util.List<String> choices;
+	@NotBlank(message = "選択肢を入力してください")
+	private String correctChoice;
 	
-	private int correctChoiceIndex;
+	@NotBlank(message = "選択肢を入力してください")
+	private String dummyChoice1;
+	
+	@NotBlank(message = "選択肢を入力してください")	
+	private String dummyChoice2;
+	
+	@NotBlank(message = "選択肢を入力してください")
+	private String dummyChoice3;
+		
 	
 	@NotBlank(message = "解説を入力してください")
 	private String annotation;
@@ -36,18 +40,16 @@ public class Problem {
 	private com.example.model.Category category;
 	
 	public Problem() {		
-		this.choices = new ArrayList<>();
-		for (int i=0; i<4; i++) {
-			this.choices.add(new String());
-		}
 	}
 	
-	public Problem(String question, List<String> choices, int correctChoiceIndex, String annotation,
+	public Problem(String question, String correctChoice, String dummyChoice1, String dummyChoice2, String dummyChoice3, int correctChoiceIndex, String annotation,
 			Category category) {
 		super();
 		this.question = question;
-		this.choices = choices;
-		this.correctChoiceIndex = correctChoiceIndex;
+		this.correctChoice = correctChoice;
+		this.dummyChoice1 = dummyChoice1;
+		this.dummyChoice2 = dummyChoice2;
+		this.dummyChoice3 = dummyChoice3;
 		this.annotation = annotation;
 		this.category = category;
 	}	
